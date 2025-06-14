18229ede45cd7141ba230a2264082ef94837d4fd
Ari Mogi <arimogi@yahoo.com>
2025-06-14 12:15:56 +0700

    Update SMD script.

--- scripts/run_SMD.sh
+++ scripts/run_SMD.sh
@@ -8,10 +8,11 @@ python run.py \
   --root_path ./dataset/SMD \
   --seq_len 100 \
   --label_len 0 \
-  --pred_len 0 \
+  --pred_len 100 \
   --e_layers 2 \
   --d_layers 1 \
   --factor 3 \
   --enc_in 38 --dec_in 38 --c_out 38 \
   --des 'anomaly_test' \
   --itr 1
+
