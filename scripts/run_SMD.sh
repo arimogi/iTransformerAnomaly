57f95b09bdb591f893d85f56447de1b60c9f122c
Ari Mogi <arimogi@yahoo.com>
2025-06-14 11:58:47 +0700

    Fix running script for dataset folder.

---
+++ scripts/run_SMD.sh
@@ -0,0 +1,17 @@
+export CUDA_VISIBLE_DEVICES=0
+
+python run.py \
+  --is_training 0 \
+  --model_id SMD-test \
+  --model iTransformer \
+  --data SMD \
+  --root_path ./dataset/SMD \
+  --seq_len 100 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 38 --dec_in 38 --c_out 38 \
+  --des 'anomaly_test' \
+  --itr 1
